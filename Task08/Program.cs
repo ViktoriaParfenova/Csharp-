97da32cef682524882439d9761ec2d4418e504f3
unknown <parfenova-viktoria@yandex.ru>
2024-03-15 23:32:42 +0300

    добавила вторую практику2

---
+++ Task08/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу которая на вход принимает число и проверяет кратно ли оно одновременно 7 и 23
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine(" да ");
+}
+else
+{
+    Console.WriteLine(" нет ");
+}
---
+++ Task09/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу которая принимает на вход 2 числа  и проверяет является ли одно квадратом другого
+
+Console.WriteLine("Введите 2 числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (Math.Pow(num1, 2) == num2 || Math.Pow(num2, 2) == num1 )
+{
+    Console.WriteLine(" да ");
+}
+else
+{
+    Console.WriteLine(" нет ");
+}
