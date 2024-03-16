b90403faa8db058ba0c220de98ab51b1cd909e28
unknown <parfenova-viktoria@yandex.ru>
2024-03-16 22:17:49 +0300

    дз вторая часть

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -1,1 +1,12 @@
-﻿// 
+﻿//  Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите числo: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (N > 0)
+{
+ count = N % 10;
+ N = N / 10;
+ System.Console.Write(count + ",");
+ 
+}
